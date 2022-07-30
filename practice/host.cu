92f83a2654b7301bf3a06a0af8cc875c0684ac67
Sohail Siadatnejad <sohale@gmail.com>
2022-07-30 19:05:32 +0100

    format new files

--- practice/host.cu
+++ practice/host.cu
@@ -1,23 +1,25 @@
 #include <stdio.h>
 const int N = 1000; // can we have a shared const on both host and device?
 
-__global__ void cuda_hello(float* gpu_ptr){
-    //printf("Hello World from GPU!\n");
-    for(int i=0;i<N;i++) {
-	    gpu_ptr[i] += 1.1;
+__global__ void cuda_hello(float *gpu_ptr)
+{
+    // printf("Hello World from GPU!\n");
+    for (int i = 0; i < N; i++)
+    {
+        gpu_ptr[i] += 1.1;
     }
 }
 
-#define output_ptr(gpu_ptr) ((void**)&gpu_ptr)
+#define output_ptr(gpu_ptr) ((void **)&gpu_ptr)
 
-int main() {
-	float* gpu_ptr = 0;
-	const int TS = sizeof(*gpu_ptr);
-	// const int N = 1000;
-        cudaMalloc( output_ptr(gpu_ptr) , N*TS);
-    	cuda_hello<<<1,1>>>(gpu_ptr);
+int main()
+{
+    float *gpu_ptr = 0;
+    const int TS = sizeof(*gpu_ptr);
+    // const int N = 1000;
+    cudaMalloc(output_ptr(gpu_ptr), N * TS);
+    cuda_hello<<<1, 1>>>(gpu_ptr);
 
-        cudaFree(gpu_ptr);	
+    cudaFree(gpu_ptr);
     return 0;
 }
-
--- scripts/compile.bash
+++ scripts/compile.bash
@@ -3,8 +3,7 @@ set -exu
 mkdir -p build
 
 docker run -it -v $(pwd):$(pwd) -w $(pwd) nvidia/cuda:11.7.0-devel-ubuntu20.04 \
-   nvcc \
-   cdf_kernel.cu \
-   practice/host.cu \
-   -o ./build/output.out
-
+    nvcc \
+    cdf_kernel.cu \
+    practice/host.cu \
+    -o ./build/output.out
