c5ce33bc85fc8b7595a407665c81c9141fef511f
Sohail Siadatnejad <sohale@gmail.com>
2022-07-30 19:29:59 +0100

    fix the bash script

--- scripts/compile.bash
+++ scripts/compile.bash
@@ -2,8 +2,10 @@ set -exu
 
 mkdir -p build
 
-docker run -it -v $(pwd):$(pwd) -w $(pwd) nvidia/cuda:11.7.0-devel-ubuntu20.04 \
-    nvcc \
-    cdf_kernel.cu \
-    practice/host.cu \
-    -o ./build/output.out
+docker run \
+    --rm -it -v $(pwd):$(pwd) -w $(pwd) \
+    nvidia/cuda:11.7.0-devel-ubuntu20.04 \
+        nvcc \
+            mvncdf_kernel.cu \
+            practice/host.cu \
+            -o ./build/output.out
