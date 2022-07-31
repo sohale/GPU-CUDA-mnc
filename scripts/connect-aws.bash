5e6f4590d1529381016660658f33b089f663bdca
Sohail Siadatnejad <sohale@gmail.com>
2022-07-31 19:16:25 +0100

    aws script but AWS didnt provide nvidia GPUs

---
+++ scripts/connect-aws.bash
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# ssh -i "./scripts/sosiaws1mykey.cer" ssssaws1@ec2-52-32-111-129.us-west-2.compute.amazonaws.com
+
+# ip-172-31-2-161.us-west-2.compute.internal
+
+# ssh -i "./scripts/sosiaws1mykey.cer" ssssaws1@52.32.111.129
+
+#ssh -i "./scripts/sosiaws1keys2.cer" ssssaws1@52.32.111.129
+
+# new instance:
+ssh -i "./scripts/sosiaws1mykey.cer" ubuntu@18.237.218.99
+# worked!
