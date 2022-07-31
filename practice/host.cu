29182eb8c9961f02c785812b9312b8aaee5de176
Sohail Siadatnejad <sohale@gmail.com>
2022-07-31 17:21:00 +0100

    format C++ code

--- practice/host.cu
+++ practice/host.cu
@@ -15,8 +15,9 @@ __global__ void cuda_simple(float *gpu_ptr, int n)
 }
 
 // inplace
-template<typename ElemType, typename SizeT>
-void process1(ElemType * gpu_ptr, SizeT n) {
+template <typename ElemType, typename SizeT>
+void process1(ElemType *gpu_ptr, SizeT n)
+{
     for (SizeT i = 0; i < n; i++)
     {
         // gpu_ptr[i] *= 1.5f;
@@ -32,8 +33,8 @@ __global__ void cuda_process_parallel(float *gpu_ptr, int n)
     // (xi, yi)  ∈  nx × ny
     const int xi = threadIdx.x;
     const int yi = blockIdx.x;
-    const int nx = blockDim.x;  // 256
-    const int ny = gridDim.x;   // 4?
+    const int nx = blockDim.x; // 256
+    const int ny = gridDim.x;  // 4?
 
     // threadIdx.x < blockDim.x = 256
     // blockIdx.x < gridDim.x = 4 (?)
@@ -48,7 +49,7 @@ __global__ void cuda_process_parallel(float *gpu_ptr, int n)
     // if (ny==1):
     int stride = nx;
     int begin = xi;
-    for (int i = begin; i < n; i+=stride)
+    for (int i = begin; i < n; i += stride)
     {
         // gpu_ptr[i] *= 1.5f;
         gpu_ptr[i] = (float)i;
@@ -68,34 +69,39 @@ void cpu_process(float *cpu_ptr, int n)
 
 #define output_ptr(gpu_ptr) ((void **)&gpu_ptr)
 
-float sum_elements(float* cpu_ptr, int n) {
+float sum_elements(float *cpu_ptr, int n)
+{
     float sum = 0.0f;
-    for(int i = 0; i < n; i++) {
+    for (int i = 0; i < n; i++)
+    {
         sum += dT * cpu_ptr[i];
     }
     return sum;
 }
 
-void cpu_print_elements(float *cpu_ptr, int n, char const * prefix_text) {
+void cpu_print_elements(float *cpu_ptr, int n, char const *prefix_text)
+{
     printf("%s ", prefix_text);
-    int begin = (n > 0) ? 0 : (N-(-n));
-    int end   = (n > 0) ? n : N;
+    int begin = (n > 0) ? 0 : (N - (-n));
+    int end = (n > 0) ? n : N;
     printf("[%d-%d]: ", begin, end);
-    for(int i = begin; i < end; i++) {
-       printf("%f ", cpu_ptr[i]);
+    for (int i = begin; i < end; i++)
+    {
+        printf("%f ", cpu_ptr[i]);
     }
     printf("\n");
 }
 
 int main()
 {
-    float* cpu_ptr = (float*)malloc(N * sizeof(float));
+    float *cpu_ptr = (float *)malloc(N * sizeof(float));
 
     float f = 10.0f;
     float pi2 = 2 * 3.141592653589793238f * f;
-    for(int i = 0; i < N; i++) {
+    for (int i = 0; i < N; i++)
+    {
         float t = i * dT;
-	cpu_ptr[i] = sin(t * pi2 * f) * 10000.0f;
+        cpu_ptr[i] = sin(t * pi2 * f) * 10000.0f;
     }
     cpu_print_elements(cpu_ptr, -10, "initial    ");
     printf("\n (pre) sum = %f\n", sum_elements(cpu_ptr, N));
@@ -105,26 +111,26 @@ int main()
     const int TS = sizeof(*gpu_ptr);
 
     cudaMalloc(output_ptr(gpu_ptr), N * TS);
-    cudaMemcpy(gpu_ptr, /*src*/cpu_ptr, N * TS, cudaMemcpyHostToDevice);
+    cudaMemcpy(gpu_ptr, /*src*/ cpu_ptr, N * TS, cudaMemcpyHostToDevice);
 
-       printf("\n (p2) sum = %f\n", sum_elements(cpu_ptr, N));
-       printf("cuda\n");
+    printf("\n (p2) sum = %f\n", sum_elements(cpu_ptr, N));
+    printf("cuda\n");
 
-       /* which is used? (1,1) versus N? */
-       cuda_simple<<<1, 1>>>(gpu_ptr, (int)N);
+    /* which is used? (1,1) versus N? */
+    cuda_simple<<<1, 1>>>(gpu_ptr, (int)N);
 
-       printf("\n (p3) sum = %f\n", sum_elements(cpu_ptr, N));
+    printf("\n (p3) sum = %f\n", sum_elements(cpu_ptr, N));
 
-       cpu_print_elements(cpu_ptr, -10, "just before");
-       printf("\n (p3b) sum = %f\n", sum_elements(cpu_ptr, N));
-       // cpu_process(cpu_ptr,N);
-       printf("\n (p3c) sum = %f\n", sum_elements(cpu_ptr, N));
-       cpu_print_elements(cpu_ptr, -10, "just after ");
-       // cpu_process(cpu_ptr,N);
+    cpu_print_elements(cpu_ptr, -10, "just before");
+    printf("\n (p3b) sum = %f\n", sum_elements(cpu_ptr, N));
+    // cpu_process(cpu_ptr,N);
+    printf("\n (p3c) sum = %f\n", sum_elements(cpu_ptr, N));
+    cpu_print_elements(cpu_ptr, -10, "just after ");
+    // cpu_process(cpu_ptr,N);
 
-       printf("\n (p4) sum = %f\n", sum_elements(cpu_ptr, N));
+    printf("\n (p4) sum = %f\n", sum_elements(cpu_ptr, N));
 
-    cudaMemcpy(/*dst*/cpu_ptr, gpu_ptr, N * TS, cudaMemcpyDeviceToHost);
+    cudaMemcpy(/*dst*/ cpu_ptr, gpu_ptr, N * TS, cudaMemcpyDeviceToHost);
     cpu_print_elements(cpu_ptr, -10, "end1        ");
     printf("\n (p5) sum = %f\n", sum_elements(cpu_ptr, N));
     cudaFree(gpu_ptr);
